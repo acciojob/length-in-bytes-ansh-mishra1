6cee20d56c6fe35f887e3320c624ee0c42a3123e
Ansh Mishra <imansh045@gmail.com>
2025-06-28 17:14:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const byteSize = (str) => {
   // write your code here
-	return str.length
+	const Blob = {
+		size: str.length
+	}
+	return Blob.size;
 };
 
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
+
